311e6d9cf2e2192bcd4d81ea28d28c518359c434
Manuel Otero Barbasán <manolo.ob@hotmail.com>
2023-08-06 01:06:07 +0200

    bug fix

--- Assets/Code/Scripts/Health.cs
+++ Assets/Code/Scripts/Health.cs
@@ -18,7 +18,7 @@ public class Health : MonoBehaviour
             return;
         }
         currentHealth -= damageAmount;
-        Debug.Log(currentHealth)
+        Debug.Log(currentHealth);
 
     }
 

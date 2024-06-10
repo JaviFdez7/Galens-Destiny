9bf3c64621ec7e2489cbe312a28bf7e314869243
Manuel Otero Barbasán <manolo.ob@hotmail.com>
2024-06-10 09:39:40 +0200

    feat: Main game new camera script

--- Assets/Code/Scripts/FollowCamera.cs
+++ Assets/Code/Scripts/FollowCamera.cs
@@ -1,63 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class FollowCamera : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
-    public GameObject target;
+    public float followSpeed = 5f; // Speed at which the camera follows
+    private Transform player; // Reference to the player's transform
 
-    private float targetPosX;
-    private float targetPosY;
+    private Camera cam; // Reference to the camera
 
-    private float posX;
-    private float posY;
+    private static List<Shake> screenShakes = new List<Shake>();
 
-    public float rightMax;
-    public float leftMax;
-    public float highMax;
-    public float highMin;
-
-    public float speed;
-    public bool on = true;
-    private float cameraZ = -500;
-
-
-    // Use this for initialization
-    void Awake()
+    public static void ScreenShake(float duration, float magnitude)
     {
-        posX = targetPosX + leftMax;
-        posY = targetPosY + highMin;
-        transform.position = Vector3.Lerp(transform.position, new Vector3(posX,posY,transform.position.z),1);
+        screenShakes.Add(new Shake(duration, magnitude));
 
     }
-
-    void MoveCam()
+    public class Shake
     {
-        if(on)
+        public float duration;
+        public float magnitude;
+
+        public Shake(float duration, float magnitude)
         {
-            if(target)
-            {
-                targetPosX = target.transform.position.x;
-                targetPosY = target.transform.position.y;
-
-                if(targetPosX > leftMax && targetPosX < rightMax)
-                {
-                    posX = targetPosX;
-                }
-                
-                if(targetPosY < highMax && targetPosY > highMin)
-                {
-                    posY = targetPosY;
-                }
-
-                transform.position = Vector3.Lerp(transform.position, new Vector3(posX,posY,cameraZ), speed * Time.deltaTime);
-            }
+            this.duration = duration;
+            this.magnitude = magnitude;
         }
     }
 
-    // Update is called once per frame
+
+    private IEnumerator ShakeCamera(float intensity, float duration)
+    {
+        Vector3 originalPosition = Camera.main.transform.position;
+        Vector3 lastCameraMovement = Vector3.zero;
+        intensity = Mathf.Clamp(intensity, 0f, 1f)/10;
+
+        while (duration > 0f)
+        {
+            duration -= Time.unscaledDeltaTime;
+            Vector3 randomMovement = new Vector3(
+                Random.Range(-1f, 1f), 
+                Random.Range(-1f, 1f)
+            ).normalized * intensity;
+            Camera.main.transform.position = Camera.main.transform.position - lastCameraMovement + randomMovement;
+            lastCameraMovement = randomMovement;
+            yield return null;
+        }
+
+        Camera.main.transform.position = originalPosition;
+    }
+
+    void Start()
+    {
+        // Find the player by the tag "Player"
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null)
+        {
+            player = playerObj.transform;
+        }
+        else
+        {
+            Debug.LogError("Player with tag 'Player' not found.");
+        }
+
+        // Get the camera component attached to this script
+        cam = GetComponent<Camera>();
+        if (cam == null)
+        {
+            Debug.LogError("Script must be attached to a camera object.");
+        }
+    }
+
     void Update()
     {
-        MoveCam();
+        if (player == null)
+        {
+            return;
+        }
+        if (screenShakes.Count > 0)
+        {
+            StartCoroutine(ShakeCamera(screenShakes[0].duration, screenShakes[0].magnitude));
+            screenShakes.RemoveAt(0);
+        }
+
+        if (Input.GetMouseButtonDown(0)){
+            ScreenShake(0.1f, 0.5f);
+        }
+
+        // Get the mouse position in the world
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = cam.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, cam.nearClipPlane));
+
+        // Calculate the midpoint between the player and the mouse position
+        Vector3 midpoint = (3*player.position + mousePosition) / 4f;
+
+        // Smoothly move the camera to the midpoint
+        Vector3 newPosition = Vector3.Lerp(transform.position, new Vector3(midpoint.x, midpoint.y, transform.position.z), followSpeed * Time.deltaTime);
+        transform.position = newPosition;
     }
 }

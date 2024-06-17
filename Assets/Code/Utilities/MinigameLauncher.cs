6e41edb228079648a689c42b5d6f2d4b72e91bfc
Manuel Otero Barbasán <manolo.ob@hotmail.com>
2024-06-17 20:02:04 +0200

    feat: new minigame zone for main game

--- Assets/Code/Utilities/MinigameLauncher.cs
+++ Assets/Code/Utilities/MinigameLauncher.cs
@@ -13,31 +13,31 @@ public class ZoneDetection : MonoBehaviour
     // Drag in the editor the text or image UI you want to appear.
     public Canvas interactionUI;
     private readonly string TagToDetect = "Player";
-    private MappeableInput input = null;
+    public MappeableInput input;
     [SerializeField] private string SceneName = "";
     [SerializeField] private int history = 1;
     [SerializeField] private SkillEnum skillNameForUnlock = SkillEnum.None;
 
+    private bool isPlayerDetected = false;
+
     private void Awake()
     {
-        input = new MappeableInput();
         interactionUI.gameObject.SetActive(false);
+        input = new MappeableInput();
+        input.InGame.Interact.Enable();
+        input.InGame.Interact.started += EnterMinigame;
     }
 
     private void OnEnable()
     {
-        input.InGame.Interact.Enable();
-        //Subscribe
         input.InGame.Interact.started += EnterMinigame;
     }
 
     private void OnDisable()
     {
-        input.Disable();
-        //Unsubscribe
         input.InGame.Interact.started -= EnterMinigame;
-
     }
+
 
 
    
@@ -47,6 +47,7 @@ public class ZoneDetection : MonoBehaviour
         {
         //SHOW UI    
         interactionUI.gameObject.SetActive(true);
+        isPlayerDetected = true;
         }
     }
 
@@ -57,11 +58,14 @@ public class ZoneDetection : MonoBehaviour
         {
             //HIDE UI
             interactionUI.gameObject.SetActive(false);
+            isPlayerDetected = false;
         }
     }
 
     private void EnterMinigame(InputAction.CallbackContext inputContext)
     {
+        Debug.Log("Enter Minigame");
+        if(!isPlayerDetected) return;
         MinigameSetupData.skillName = skillNameForUnlock;
         MinigameSetupData.history = history;
         SceneLoader.LoadSceneAsyncWithLoadingBar(SceneName, true);
